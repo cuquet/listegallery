a4df9a79d674db38643c5f24aa6b4b1504d14f38
cuquet@gmail.com <cuquet@gmail.com@dfab4ed4-a92c-11de-80ce-d38665a2569c>
2009-10-12 20:44:03 +0000

    Update feed construction with a class

--- feed.php
+++ feed.php
@@ -9,56 +9,60 @@
 * 				http://www.gnu.org/licenses/gpl.html
 *	@comments:	RSS Feed Creator
 **********************************************************/
-include_once("includes/listen_functions.php"); 
-
-mp3act_connect();
-
-header("Content-Type: text/xml");
-    $error = "";
-    $query = "SELECT ".tableName("albums.album_name").", ".tableName("albums.album_art").", ".
-                                     tableName("artists.artist_name").", ".tableName("artists.prefix").", ".
-                                     tableName("songs.date_entered")." as [pubdate] ".   
-                                     "FROM ".tableName("songs").", ".tableName("albums").", ".tableName("artists")." ".
-                                     "WHERE ".tableName("songs.album_id")." = ".tableName("albums.album_id")." ".
-                                     "AND ".tableName("artists.artist_id")." = ".tableName("songs.artist_id")." ".
-                                     "GROUP BY ".tableName("songs.album_id")." ORDER BY ".tableName("songs.date_entered")." DESC LIMIT 10";
-    $results = getAllResultsForQuery($query, $error);
-    echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
-?>
-
-<rss version="2.0">
-
-<channel>
-	<title>mp3act - Recently Added Albums</title>
-	<pubDate>Fri, 03 Jun 2005 10:23:45 +0000</pubDate>
-	<link><?php echo $GLOBALS["http_url"].$GLOBALS["uri_path"]."/"; ?></link>
-	<description>A list of the 10 most recently added music albums to this mp3act server.</description>
-	<generator><?php echo $GLOBALS["http_url"].$GLOBALS["uri_path"]."/feed.php"; ?></generator>
-	<language>en</language>
-<?php
-foreach($results as $row) 
+class RSS
 {
-?>
+	public function RSS()
+	{
+		require_once("includes/listen_functions.php"); 
+	}
+	private function dbConnect()
+	{
+		createConnection();
+		dibi::addSubst('', $GLOBALS["db_prefix"]);
+	}
+	public function GetFeed()
+	{
+		return $this->getDetails() . $this->getItems();
+	}
+	private function getDetails()
+	{
+		$details = '<?xml version="1.0" encoding="ISO-8859-1" ?>
+					<rss version="2.0">
+						<channel>
+							<title>'.$GLOBALS["server_title"].' '.getSystemSetting("version").' - Recently Added Albums</title>
+							<link>'. $GLOBALS["http_url"].$GLOBALS["uri_path"].'/</link>
+							<description>A list of the 10 most recently added music albums to thisserver.</description>
+							<language>en</language>';
+		return $details;
+	}
+	
+	private function getItems()
+	{
+		$query = "SELECT [song_id],[name],[album_id], [album_name], [artist_name], [prefix], [date_entered] AS [pubdate] ".   
+             "FROM [::songs] INNER JOIN [::artists] USING ([artist_id]) INNER JOIN [::albums] USING ([album_id]) ".
+             "GROUP BY [album_id] ORDER BY [date_entered] DESC LIMIT 10";
+		$itemsTable = "webref_rss_items";
+		$this->dbConnect($itemsTable);
+		$results = dibi::query($query)->fetchAll();
+		$items = '';
+		foreach($results as $row)
+		{
+			$items .= '<item>
+						 <title>'. htmlentities($row["name"]).'</title>
+						 <pubDate>'.$row["pubdate"].'</pubDate>
+						 <description><![CDATA['.
+							'<img style="float:left background: #fff; border:1px solid #999; margin:0; padding:3px;" src="image.php?id='. $row["album_id"] .'&thumb=1&rand='.mt_rand().'" />'.
+							'<p style="margin:0; border-bottom: 5px solid #aaa; border-top: 5px solid #aaa; padding: 8px;"><strong>Artist:</strong> '.$row["prefix"].htmlentities($row["artist_name"]).'<br/><strong>Album:</strong> '.htmlentities($row["album_name"]).'</p>'.
+						 ']]></description>
+					 </item>';
+		}
+		$items .= '</channel>
+				 </rss>';
+		return $items;
+	}
 
-<item>
-<title><?php echo $row["prefix"]." ".htmlentities($row['artist_name'])." - ".htmlentities($row['album_name']); ?></title>
-<description>	<![CDATA[<?php echo "<p ><strong>Artist:</strong> $row[prefix]".htmlentities($row['artist_name'])."<br/><strong>Album:</strong> ".htmlentities($row['album_name']); 
-		if($row['album_art'] && $row['album_art'] != 'fail' ) { echo "<br/><img src=\"".$GLOBALS["http_url"].$GLOBALS["uri_path"]."/art/".$row['album_art']."\" />"; }
-	?>
-	</p>]]></description>
-	<? ($GLOBALS["db_access"]["driver"]=="mysql" ? $date= strtotime($row['pubdate']) :$date=$row['pubdate']); ?>
-<pubDate><?php echo date("r", $date); ?></pubDate>
-<content:encoded>
-	<![CDATA[<?php echo "<p style=\"margin:0; border-bottom: 8px solid #aaa; border-top: 8px solid #aaa; padding: 8px; background: #ddd; \"><strong>Artist:</strong> ".$row['prefix']." ".htmlentities($row['artist_name'])."<br/><strong>Album:</strong> ".htmlentities($row['album_name']); 
-		if($row['album_art'] && $row['album_art'] != 'fail' ) { echo "<br/><img style=\"background: #fff; border:1px solid #999; margin:0; padding:3px; \" src=\"".$GLOBALS["http_url"].$GLOBALS["uri_path"]."/art/".$row['album_art']."\" />"; }
-	?>
-	</p>]]>
-</content:encoded>
-<link>http://mp3act.net</link>
-</item>
-<?php
 }
+header("Content-Type: application/xml; charset=ISO-8859-1");
+$rss = new RSS();
+echo $rss->GetFeed();
 ?>
-</channel>
-
-</rss>
